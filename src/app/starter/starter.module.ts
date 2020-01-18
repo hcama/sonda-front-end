e3f0eba7cd975aab211289730739f011bc6d9817
Hcama <hcama1985@outlook.com>
2020-01-17 23:47:00 -0500

    Initial commit

---
+++ src/app/starter/starter.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { StarterComponent } from './starter.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Mantenimiento de Usuarios',
+      urls: [
+        { title: 'Mantenimiento', url: '/dashboard' },
+        { title: 'Usuario' }
+      ]
+    },
+    component: StarterComponent
+  }
+];
+
+@NgModule({
+  imports: [FormsModule, CommonModule, RouterModule.forChild(routes)],
+  declarations: [StarterComponent]
+})
+export class StarterModule {}
